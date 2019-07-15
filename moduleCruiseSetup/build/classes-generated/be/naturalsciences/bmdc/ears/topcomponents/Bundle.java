5ab4e87b9f9397b9fedd6ac528acc36228d02945
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2019-07-15 12:03:33 +0200

    add build files to correct yvans mistake

---
+++ moduleCruiseSetup/build/classes-generated/be/naturalsciences/bmdc/ears/topcomponents/Bundle.java
@@ -0,0 +1,153 @@
+package be.naturalsciences.bmdc.ears.topcomponents;
+/** Localizable strings for {@link be.naturalsciences.bmdc.ears.topcomponents}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Create new cruise...</i>
+     * @see CreateCruiseSetupTopComponent
+     */
+    static String CTL_CreateCruiseSetupAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateCruiseSetupAction");
+    }
+    /**
+     * @return <i>Create new cruise</i>
+     * @see CreateCruiseSetupTopComponent
+     */
+    static String CTL_CreateCruiseSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateCruiseSetupTopComponent");
+    }
+    /**
+     * @return <i>Create/edit events...</i>
+     * @see CreateEventTopComponent
+     */
+    static String CTL_CreateEventAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateEventAction");
+    }
+    /**
+     * @return <i>Create/edit events</i>
+     * @see CreateEventTopComponent
+     */
+    static String CTL_CreateEventTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateEventTopComponent");
+    }
+    /**
+     * @return <i>Create new program...</i>
+     * @see CreateProgramSetupTopComponent
+     */
+    static String CTL_CreateProgramSetupAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateProgramSetupAction");
+    }
+    /**
+     * @return <i>Create new program</i>
+     * @see CreateProgramSetupTopComponent
+     */
+    static String CTL_CreateProgramSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CreateProgramSetupTopComponent");
+    }
+    /**
+     * @return <i>Edit Cruise</i>
+     * @see EditCruiseSetupTopComponent
+     */
+    static String CTL_EditCruiseSetupAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_EditCruiseSetupAction");
+    }
+    /**
+     * @return <i>Edit Cruise on </i>
+     * @see EditCruiseSetupTopComponent
+     */
+    static String CTL_EditCruiseSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_EditCruiseSetupTopComponent");
+    }
+    /**
+     * @return <i>Edit Program</i>
+     * @see EditProgramSetupTopComponent
+     */
+    static String CTL_EditProgramSetupAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_EditProgramSetupAction");
+    }
+    /**
+     * @return <i>EditProgramSetup Window</i>
+     * @see EditProgramSetupTopComponent
+     */
+    static String CTL_EditProgramSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_EditProgramSetupTopComponent");
+    }
+    /**
+     * @return <i>Edit cruise...</i>
+     * @see UpdateCruiseTopComponent
+     */
+    static String CTL_UpdateCruiseAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_UpdateCruiseAction");
+    }
+    /**
+     * @return <i>Search for cruises</i>
+     * @see UpdateCruiseTopComponent
+     */
+    static String CTL_UpdateCruiseTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_UpdateCruiseTopComponent");
+    }
+    /**
+     * @return <i>Edit program...</i>
+     * @see UpdateProgramTopComponent
+     */
+    static String CTL_UpdateProgramAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_UpdateProgramAction");
+    }
+    /**
+     * @return <i>Search for programs</i>
+     * @see UpdateProgramTopComponent
+     */
+    static String CTL_UpdateProgramTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_UpdateProgramTopComponent");
+    }
+    /**
+     * @return <i>Create a new program by choosing scientist, harbour,...</i>
+     * @see CreateCruiseSetupTopComponent
+     */
+    static String HINT_CreateCruiseSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CreateCruiseSetupTopComponent");
+    }
+    /**
+     * @return <i>Manage events</i>
+     * @see CreateEventTopComponent
+     */
+    static String HINT_CreateEventTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CreateEventTopComponent");
+    }
+    /**
+     * @return <i>Create a new program by choosing cruise, PI,...</i>
+     * @see CreateProgramSetupTopComponent
+     */
+    static String HINT_CreateProgramSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CreateProgramSetupTopComponent");
+    }
+    /**
+     * @return <i>This is a Edit Cruise Setup window</i>
+     * @see EditCruiseSetupTopComponent
+     */
+    static String HINT_EditCruiseSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_EditCruiseSetupTopComponent");
+    }
+    /**
+     * @return <i>This is a EditProgramSetup window</i>
+     * @see EditProgramSetupTopComponent
+     */
+    static String HINT_EditProgramSetupTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_EditProgramSetupTopComponent");
+    }
+    /**
+     * @return <i>Search for cruises</i>
+     * @see UpdateCruiseTopComponent
+     */
+    static String HINT_UpdateCruiseTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_UpdateCruiseTopComponent");
+    }
+    /**
+     * @return <i>Search for programs</i>
+     * @see UpdateProgramTopComponent
+     */
+    static String HINT_UpdateProgramTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_UpdateProgramTopComponent");
+    }
+    private Bundle() {}
+}
